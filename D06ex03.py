5cc03a4cc929ace9d72ea23d3249f4cf7f911b9b
ghvik <vreddy@ischool.berkeley.edu>
2016-08-09 12:34:51 -0700

    Finished D06ex04, writing e names to a file

--- D06ex03.py
+++ D06ex03.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python
 # D06ex03.py
-# Write 10 random numbers to a file
-########################################################################
+# Reading from a text file
+#
+####################################################################################
 
-from random import randint
+def how_many_e(file_name):
+    """Counts how many e's appear in the file
+    
+    Parameter:
+    file_name -- the given file to read
+    """
+    count = 0
+    with open(file_name) as fin:
+        for line in fin:
+            for letter in line.split()[0]:
+                if letter == 'e':
+                    count += 1
+                
+    print("There are {} instances of the letter 'e'.".format(count))
 
+def names_with_e(file_name):
+    """Prints the names with at least one 'e'
+    
+    Parameter:
+    file_name -- the given file to read
+    """
+    words_with_e = []
+    with open(file_name) as fin:
+        for line in fin:
+            has_an_e = False
+            for letter in line.split()[0]:
+                if letter == 'e':
+                    has_an_e = True
+            if has_an_e:
+                words_with_e.append(line.split()[0])
 
-def write_random_numbers():
-    for n in range(10):
-        print(randint(1,50))
-        
+    print("These are the names with at least one 'e': {}".format(words_with_e))
+
 def main():
-    write_random_numbers()
+    how_many_e("roster.txt")
+    names_with_e("roster.txt")
     
 if __name__ == "__main__":
     main()
--- D06ex04.py
+++ D06ex04.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
 # D06ex04.py
-# Reading from a text file
+# Reading from a text file and writing to another file
 #
 ####################################################################################
 
-def how_many_e(file_name):
+def how_many_e(file_in, file_out):
     """Counts how many e's appear in the file
     
     Parameter:
-    file_name -- the given file to read
+    file_in -- the given file to read
+    file_out -- the file name to write output to
     """
     count = 0
-    with open(file_name) as fin:
+    with open(file_in) as fin:
         for line in fin:
             for letter in line.split()[0]:
                 if letter == 'e':
                     count += 1
-                
-    print("There are {} instances of the letter 'e'.".format(count))
+    
+    with open(file_out, 'w') as fout:
+        fout.write("There are {} instances of the letter 'e'.\n".format(count))
 
-def names_with_e(file_name):
+def names_with_e(file_in, file_out):
     """Prints the names with at least one 'e'
     
     Parameter:
     file_name -- the given file to read
+    file_out -- the file name to write output to
     """
     words_with_e = []
-    with open(file_name) as fin:
+    with open(file_in) as fin:
         for line in fin:
             has_an_e = False
             for letter in line.split()[0]:
@@ -34,12 +37,13 @@ def names_with_e(file_name):
                     has_an_e = True
             if has_an_e:
                 words_with_e.append(line.split()[0])
-
-    print("These are the names with at least one 'e': {}".format(words_with_e))
+                
+    with open(file_out, 'a') as fout:
+        fout.write("These are the names with at least one 'e': {}".format(words_with_e))
 
 def main():
-    how_many_e("roster.txt")
-    names_with_e("roster.txt")
+    how_many_e("roster.txt", "names_with_e.txt")
+    names_with_e("roster.txt", "names_with_e.txt")
     
 if __name__ == "__main__":
     main()
