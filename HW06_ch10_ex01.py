80e072c57bac62c57cce968dd4bd92ef6cfb1c51
ghvik <vreddy@ischool.berkeley.edu>
2016-08-09 17:24:23 -0700

    Finished ch10 ex01, nested sum of numbers in a nested loop

--- HW06_ch10_ex01.py
+++ HW06_ch10_ex01.py
@@ -6,16 +6,26 @@
 #  - Do not print anything extraneous!
 #  - Do not put anything but pass in main()
 
-def nested_sum(summand1, summand2):
-    summands_and_sum = []
-    summands_and_sum.extend([summand1, summand2])
-    summands_and_sum.append([sum([summand1, summand2])])
-    return summands_and_sum[2][0]
+def nested_sum(list_of_numbers, sum_of_numbers=0):
+    """Recursively finds the sum of numbers in a n-nested list
+    where n is any number, and the nests appears anywhere.
+    
+    Parameters:
+    lists_of_numbers -- the nested list of numbers
+    sum_of_numbers -- the sum of the numbers in all the nested lists
+                    (initial value = 0)
+    """
+    for obj in list_of_numbers:
+        if isinstance(obj, list):
+            sum_of_numbers = nested_sum(obj, sum_of_numbers)
+        else:
+            sum_of_numbers += obj
+    return sum_of_numbers
 
 def main():
-    print(nested_sum(1, 4))
-    print(nested_sum(321, -325))
-    print(nested_sum(-43, 43))
+    print(nested_sum([1, [4]]))
+    print(nested_sum( [1,2,3,[4,5],[-1,-2,-3,[-4,-5]],6] ))
+    print(nested_sum([-43, 43,[],[],[[]],8]))
     
 if __name__ == "__main__":
     main()
